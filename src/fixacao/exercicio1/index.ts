73c12726324f2585c10a1323ad51dbdef6521ecc
myllenavieira <vieirasilvamyllena@gmail.com>
2023-03-06 21:20:21 -0300

    Typescript II

--- src/fixacao/exercicio1/index.ts
+++ src/fixacao/exercicio1/index.ts
@@ -9,3 +9,45 @@ que todos os objetos tenham as mesmas propriedades.
 
 3. Modifique o type Pessoa para que possamos escolher apenas entre as cores do arco-íris. Utilize um enum para isso.
 */
+
+type Pessoa = {
+  nome: string,
+  idade: number,
+  corFavorita: CorFavorita
+}
+
+enum CorFavorita {
+  VERMELHO="vermelho",
+  LARANJA="laranja",
+  AMARELO="amarelo",
+  VERDE="verde",
+  AZUL="azul",
+  ANIL="anil",
+  VIOLETA="violeta"
+}
+
+const Pessoa1: Pessoa = {
+  nome: "Maria",
+  idade: 20,
+  corFavorita: CorFavorita.AMARELO
+}
+
+const Pessoa2: Pessoa = {
+  nome: "José",
+  idade: 18,
+  corFavorita: CorFavorita.AZUL
+}
+
+const Pessoa3: Pessoa = {
+  nome: "João",
+  idade: 24,
+  corFavorita: CorFavorita.LARANJA
+}
+
+const Pessoa4: Pessoa = {
+  nome: "Júlia",
+  idade: 19,
+  corFavorita: CorFavorita.VERMELHO
+}
+
+console.table([Pessoa1, Pessoa2, Pessoa3, Pessoa4]);
